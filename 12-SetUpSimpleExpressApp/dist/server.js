ca9bd1d0ca21134843a13c2510de17bb9faf335c
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-04-22 16:46:42 +0200

    Exercise 12

--- 12-SetUpSimpleExpressApp/dist/server.js
+++ 12-SetUpSimpleExpressApp/dist/server.js
@@ -4,8 +4,30 @@ import morgan from "morgan";
 const app = express();
 const port = 3000;
 app.use(morgan("dev"));
-app.get("/", (req, res) => {
-    res.status(200).json({ msg: "Hello World!" });
+app.use(express.json());
+let planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
+app.get("/api/planets", (req, res) => {
+    res.status(200).json(planets);
+});
+app.get("/api/planets/:id", (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find((p) => p.id === Number(id));
+    res.status(200).json(planet);
+});
+app.post("/api/planets", (req, res) => {
+    const { id, name } = req.body;
+    const newPlanet = { id, name };
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: "The planet was created" });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- 12-SetUpSimpleExpressApp/src/server.ts
+++ 12-SetUpSimpleExpressApp/src/server.ts
@@ -5,9 +5,42 @@ const app = express();
 const port = 3000;
 
 app.use(morgan("dev"));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Hello World!" });
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+});
+
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json(planet);
+});
+
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+  planets = [...planets, newPlanet];
+
+  res.status(201).json({ msg: "The planet was created" });
 });
 
 app.listen(port, () => {
